3446c677f34ddc6b0fe41f58ebe8ceeb9a33081f
Javier Marin <oscarjavimarin@gmail.com>
2023-08-15 14:32:53 -0500

    proyecto integrador parte 3

---
+++ Proyecto main.py
@@ -0,0 +1,28 @@
+#Saludo y bienvenida al proyecto
+print ("Hola... por favor ingrese su nombre:")
+nombre = input()
+print(f"Bienvenido al reto {nombre}!")
+
+#Proyecto integrador parte 2
+import readchar
+while True:
+    keypress = readchar.readkey()
+    if keypress == readchar.key.UP:
+        break
+    print(keypress)
+
+#Proyecto integrador parte 3
+import os
+
+def borrar_terminal():
+    numero_de_veces = 0
+    print ("borrar terminal")
+    while numero_de_veces <50:
+        tecla = readchar.readkey()
+        if ord(tecla)>0:
+            os.system('cls' if os.name=='nt' else 'clear')
+            numero_de_veces = numero_de_veces+1
+            print (numero_de_veces)
+
+if __name__ == "__main__":
+    borrar_terminal()
---
+++ proyecto python.py
@@ -0,0 +1,4 @@
+print("Por favor ingrese su nombre:")
+nombre = input()
+print(f"Bienvenido al programa {nombre}!")
+
